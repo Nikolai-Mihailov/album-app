ac14e659a8cd0d46ae37dc8b2eb595f8d9d838dc
Nikolai Mihailov <nikolai.mihailov92@gmail.com>
2022-02-05 16:51:50 +0200

    added localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,31 @@
-import { applyMiddleware } from 'redux';
+import { applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import albumsReducer from './slices/albums';
 import galleryReducer from './slices/gallery';
 import favouritesReducer from './slices/favourites';
 
+const redusers = combineReducers({
+    albums: albumsReducer,
+    gallery: galleryReducer,
+    favourites: favouritesReducer
+})
+
+const persistConfig = {
+    key: 'favourites',
+    storage
+};
+
+const persisteReducer = persistReducer(persistConfig, redusers);
 const store = configureStore({
-    reducer: { albums: albumsReducer, gallery: galleryReducer, favourites: favouritesReducer }
+    reducer: persisteReducer,
+    middleware: [thunk]
 }, composeWithDevTools(
     applyMiddleware(thunk),
 ));
-
+export const persistor = persistStore(store);
 export default store;
