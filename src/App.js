72d07e958ab4f86f074d8e4dad63b698536dbfae
Nikolay Mihaylov <nikolaym@nvalue.local>
2022-02-09 11:18:57 +0200

    Added lazy loading for components, all css are downloaded, added Not Found page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import SpinnerComponent from "./components/UI/Spinner";
 
-import AlbumsComponent from './components/pages/AlbumsComponent';
-import GalleryComponent from './components/pages/GalleryComponent';
-import HeaderComponent from "./components/UI/Header";
-import FavouritesComponent from "./components/pages/FavouritesComponent";
+const HeaderComponent = React.lazy(() => import('./components/UI/Header'));
+const AlbumsComponent = React.lazy(() => import('./components/pages/AlbumsComponent'));
+const GalleryComponent = React.lazy(() => import('./components/pages/GalleryComponent'));
+const FavouritesComponent = React.lazy(() => import('./components/pages/FavouritesComponent'));
+const NotFoundComponent = React.lazy(() => import('./components/pages/NotFoundComponent'));
 
 function App() {
   return (
-    <div>
+    <Suspense fallback={SpinnerComponent}>
       <header>
         <HeaderComponent />
       </header>
@@ -16,9 +19,10 @@ function App() {
           <Route path="/" element={<AlbumsComponent />} />
           <Route path="/gallery/:id" element={<GalleryComponent />} />
           <Route path="/favourites" element={<FavouritesComponent />} />
+          <Route path="*" element={<NotFoundComponent />} />
         </Routes>
       </main>
-    </div>
+    </Suspense>
   );
 }
 
---
+++ src/components/pages/NotFoundComponent.js
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function NotFoundComponent() {
+    return (
+        <Box flex={1} height="80vh" display="flex" flexDirection="row" justifyContent="center" alignItems="center">
+            <h3>404 Not Found Page...</h3>
+        </Box >
+    );
+}
